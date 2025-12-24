d742963f9b03a5bcf7ae0d9447a6156882be4ea2
León Martín <leonmartinl@icloud.com>
2025-12-24 11:29:24 +0100

    implement streamed responses from AI clients to measure time to first token

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -7,9 +7,16 @@ export const generateAIResponse = async (req, res) => {
     const span = tracer.startSpan('generateAIResponse');
     try {
         const { provider, prompt } = req.body;
+        span.setAttribute('ai.provider', provider);
 
         const aiResponse = await generateText(provider, prompt);
-        res.status(200).json({ response: aiResponse });
+        
+        span.setAttribute('ai.ttft', aiResponse.metrics.timeToFirstToken);
+        span.setAttribute('ai.inference_time', aiResponse.metrics.totalTime);
+        
+        res.status(200).json({
+            response: aiResponse.text,
+        });
     } catch (error) {
         res.status(500).json({ message: `Internal server error: ${error}` });
     } finally {
--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -2,28 +2,50 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-})
+});
 
 const ollama = new OpenAI({
     baseURL: 'http://localhost:11434/v1',
     apiKey: 'ollama', // required but unused
-})
+});
+
+const providers = {
+    "openai": {
+        client: openai,
+        model: 'gpt-5-mini',
+    },
+    "ollama": {
+        client: ollama,
+        model: 'llama3',
+    }
+}
 
 export const generateText = async (provider, prompt) => {
-    let response;
+    let textResponse, firstTokenTime;
 
-    if (provider === 'openai') {
-        response = await openai.responses.create({
-            // given API key only has access to 'gpt-5-mini'
-            model: 'gpt-5-mini',
-            input: prompt,
-        });
-    } else {
-        response = await ollama.responses.create({
-            model: 'llama2',
-            input: prompt,
-        });
+    const { client, model } = providers[provider];
+
+    const startTime = Date.now();
+    const stream = await client.responses.create({
+        model: model,
+        input: prompt,
+        stream: true
+    });
+
+    for await (const event of stream) {
+        if (!firstTokenTime && event.type === 'response.output_text.delta') {
+            firstTokenTime = Date.now();
+        } 
+        else if (event.type === 'response.completed') {
+            textResponse = event.response.output.find(output => output.type === 'message').content[0].text;
+        }
     }
-    
-    return response.output_text;
+
+    return {
+        text: textResponse,
+        metrics: {
+            timeToFirstToken: firstTokenTime - startTime,
+            totalTime: Date.now() - startTime
+        }
+    }
 };
